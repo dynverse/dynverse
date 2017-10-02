ec1beea1bc4341501c66429362efb12d572154db
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-02 15:33:10 +0200

    merge dynverse and dynalysis

--- dynverse/R/package.R
+++
@@ -1,9 +0,0 @@
-#' Easy installing and loading of the dynverse
-#'
-#' The 'dynverse' is a set of packages with which to
-#' evaluate trajectory inference methods for dynamic processes.
-#'
-#' @docType package
-#' @name dynverse
-NULL
-
