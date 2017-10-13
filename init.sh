b62552a40cb4d948d41520680d28ad28e40e71bb
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-10-13 09:30:31 +0200

    updated repositories

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# for each submodule: 
+# # git submodule add -b master git@github.com:dynverse/mfa.git methods/mfa
+
 git submodule update --init --recursive
 git submodule foreach git checkout master
 git submodule foreach git pull
