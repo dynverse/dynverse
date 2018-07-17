52370b8f2a198c7a34e12ca8106f58f8d06355f1
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-17 13:09:56 +0200

    add local installer script

---
+++ install_dynpackages.R
@@ -0,0 +1,68 @@
+#!/usr/bin/Rscript
+
+library(tidyverse)
+requireNamespace("igraph")
+requireNamespace("desc")
+requireNamespace("GA")
+requireNamespace("devtools")
+
+# find local repositories
+files <- list.files(path = ".", pattern = "DESCRIPTION", recursive = TRUE, full.names = TRUE)
+
+packages <- map_df(files, function(file){
+  descr <- desc::desc(file = file)
+  package <- descr$get("Package")
+  data_frame(package, file, dir = gsub("/DESCRIPTION", "", file))
+}) %>%
+  filter(!grepl("revdep", dir)) %>%
+  as_data_frame()
+
+dependencies <- map_df(packages$file, function(file){
+  descr <- desc::desc(file = file)
+
+  package <- descr$get("Package")
+  deps <- descr$get_deps()
+
+  deps %>%
+    rename(dependency = package) %>%
+    mutate(package) %>%
+    select(package, dependency, type) %>%
+    as_data_frame()
+})
+
+# create dependency graph between dyn-packages
+depgr <- igraph::graph_from_data_frame(
+  d = dependencies %>% filter(grepl("^dyn", package), grepl("^dyn", dependency)),
+  directed = TRUE,
+  vertices = packages %>% filter(grepl("^dyn", package))
+)
+
+# calculate which nodes are connected to which, taking directionality into account
+dist <- igraph::distances(depgr, mode = "in")
+dist[is.infinite(dist)] <- 0
+dist[dist > 0] <- 1
+
+# overkill way of determining the installation order
+fit <- GA::ga(
+  type = "permutation",
+  fitness = function(ord) {
+    (sum(dist) - sum(dist[ord, ord][lower.tri(dist, diag = FALSE)])) / sum(dist)
+  },
+  nBits = nrow(dist),
+  lower = 1,
+  upper = nrow(dist),
+  maxiter = 1000,
+  popSize = 100,
+  maxFitness = 1,
+  monitor = FALSE
+)
+
+install_order <- rownames(dist)[fit@solution[1,]]
+
+ordered_packages <- packages[fit@solution[1,],]
+
+# install each package individually
+walk(seq_len(nrow(ordered_packages)), function(i) {
+  cat("Installing ", i, "/", nrow(ordered_packages), ": ", ordered_packages$package[[i]], "\n", sep = "")
+  devtools::install(pkg = ordered_packages$dir[[i]], dependencies = FALSE, quiet = TRUE)
+})
