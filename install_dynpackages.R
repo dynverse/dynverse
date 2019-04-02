1af73a5e377c8afa271632d224703363a12f0148
Robrecht Cannoodt <rcannood@gmail.com>
2019-04-02 08:28:35 +0200

    update install script to fix circular dependencies

--- install_dynpackages.R
+++ install_dynpackages.R
@@ -1,13 +1,14 @@
 #!/usr/bin/Rscript
 
-library(tidyverse)
-requireNamespace("igraph")
-requireNamespace("desc")
-requireNamespace("devtools")
+library(dplyr, warn.conflicts = FALSE)
+library(purrr, warn.conflicts = FALSE)
+library(desc, warn.conflicts = FALSE)
+library(devtools, warn.conflicts = FALSE)
+requireNamespace("igraph", quietly = TRUE)
 
 # find local repositories
-files <- 
-  list.files(path = ".", pattern = "DESCRIPTION", recursive = TRUE, full.names = TRUE) %>% 
+files <-
+  list.files(path = ".", pattern = "DESCRIPTION", recursive = TRUE, full.names = TRUE) %>%
   discard(grepl("revdep", .))
 
 packages <- map_df(files, function(file){
@@ -16,17 +17,17 @@ packages <- map_df(files, function(file){
   version <- descr$get("Version")
   tibble(package, version, file, dir = gsub("/DESCRIPTION", "", file))
 }) %>%
-  as_tibble() %>% 
+  as_tibble() %>%
   mutate(
-    colour = dynplot:::milestone_palette(name = "rainbow", n = n())
+    colour = rainbow(n())
   )
 
 dependencies <- map_df(packages$file, function(file){
   descr <- desc::desc(file = file)
-  
+
   package <- descr$get("Package")
   deps <- descr$get_deps()
-  
+
   deps %>%
     rename(dependency = package) %>%
     mutate(package) %>%
@@ -34,13 +35,14 @@ dependencies <- map_df(packages$file, function(file){
     as_tibble()
 })
 
-dyndependencies <- 
-  dependencies %>% 
-  inner_join(packages %>% select(package), by = "package") %>% 
+dyndependencies <-
+  dependencies %>%
+  inner_join(packages %>% select(package), by = "package") %>%
   inner_join(packages %>% select(dependency = package), by = "dependency")
 
 # packages %>% write_tsv("~/packages_info.tsv")
 # dyndependencies %>% write_tsv("~/packages_dependencies.tsv")
+
 
 # create dependency graph between dyn-packages
 depgr <- igraph::graph_from_data_frame(
@@ -51,14 +53,39 @@ depgr <- igraph::graph_from_data_frame(
 
 # calculate which nodes are connected to which, taking directionality into account
 dist <- igraph::distances(depgr, mode = "in")
+
+# detect circular dependencies
+circular_dependencies <- dist * t(dist) > 0
+if (any(circular_dependencies)) {
+  # create dependency graph between dyn-packages
+  depgr1 <- igraph::graph_from_data_frame(
+    d = dyndependencies %>% filter(type != "Suggests"),
+    directed = TRUE,
+    vertices = packages
+  )
+
+  # calculate which nodes are connected to which, taking directionality into account
+  dist1 <- igraph::distances(depgr1, mode = "in")
+
+  # fix circular dependencies
+  dist[circular_dependencies] <- dist1[circular_dependencies]
+}
+
 dist[is.infinite(dist)] <- 0
 dist[dist > 0] <- 1
+
+
+# check for circular dependencies
+if (any(dist * t(dist) > 0)) {
+  stop("Circular dependencies detected")
+}
 
 # determine installation order
 selected_ix <- c()
 to_select_ix <- seq_len(ncol(dist))
 
 while (length(to_select_ix) > 0) {
+  print(dist[to_select_ix, to_select_ix, drop = FALSE])
   wts <- colSums(dist[to_select_ix, to_select_ix, drop = FALSE])
   ix <- which(wts == 0)
   selected_ix <- c(selected_ix, to_select_ix[ix])
